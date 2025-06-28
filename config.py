3cfccde20774b00617b887554d03444502a971fc
Phechr2025 <duang.dees12@gmail.com>
2025-06-28 12:15:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')  # ใช้ค่าจาก environment variable เท่านั้น
+DISCORD_TOKEN = os.getenv('TOKEN')  # ใช้ค่าจาก environment variable เท่านั้น
 TARGET_CHANNEL_ID = 1386319025478434987  # ใส่ห้องที่อนุญาตให้ใช้ !ตรวจสอบ
 GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1S2Ww6PP6i2LYl2g42vAI6VRCekxLSmGCOLeYpcqkwwE/edit?usp=sharing"
 SHEET_NAME = "ชีต1"  # ตรงกับชื่อแท็บด้านล่างซ้ายของชีต
